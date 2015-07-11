ddd653c355a86a6d9e7a58afd61e0eef534c3b3e
lifsong <lifen.song@gmail.com>
2015-07-12 00:09:28 +0800

    detailed execution steps for later implementation

--- src/auction/Auction.java
+++ src/auction/Auction.java
@@ -9,31 +9,42 @@ public class Auction {
 	public void execute() {
 
 		while (true) {
+			/*
+			 * if system time is not within [9:25:00, 9:30:00) or today's
+			 * aggregate auction was done already, skip checking
+			 */
+			if (true) {/*todo: implement it*/
+				/* pre-condition: ask if the files are existing */
+				if (!ftpOp.isFtpFileExisting(FtpWrapper.getClosePriceFilename())) {
+					/* sleep for 2sec, wait for 3rd party to generate file */
+					System.out.println("ClosePriceFile does not exist!");
+					continue;
+				} else if (!ftpOp.isFtpFileExisting(FtpWrapper.getBuyingInfoFilename())) {
+					/* sleep for 2sec, wait for 3rd party to generate file */
+					System.out.println("BuyingInfoFile does not exist!");
+					continue;
+				} else if (!ftpOp.isFtpFileExisting(FtpWrapper.getSellingInfoFilename())) {
+					/* sleep for 2sec, wait for 3rd party to generate file */
+					System.out.println("SellingInfoFile does not exist!");
+					continue;
+				}
 
-			/* pre-condition: the files should be existing */
-			if (!ftpOp.isFtpFileExisting(FtpWrapper.getClosePriceFilename())) {
-				/* sleep for 2sec */
-				System.out.println("ClosePriceFile does not exist!");
-				continue;
-			} else if (!ftpOp.isFtpFileExisting(FtpWrapper.getBuyingInfoFilename())) {
-				/* sleep for 2sec */
-				System.out.println("BuyingInfoFile does not exist!");
-				continue;
-			} else if (!ftpOp.isFtpFileExisting(FtpWrapper.getSellingInfoFilename())) {
-				/* sleep for 2sec */
-				System.out.println("SellingInfoFile does not exist!");
-				continue;
+				/* get trading stocks' last day close price */
+
+				/*
+				 * filter out invalid trades if time is invalid or price is out
+				 * of pre-defined max range
+				 */
+
+				/* find stocks which have trades */
+				Set<Integer> sellingStocks = ftpOp.getSellingStocks();
+				Set<Integer> buyingStocks = ftpOp.getBuyingStocks();
+				Set<Integer> tradingStocks = findTradingStocks(sellingStocks, buyingStocks);
+
+				/* do the aggregation auction if tradingStocks has content */
+
+				/* update the matrix screen */
 			}
-
-			Set<Integer> sellingStocks = ftpOp.getSellingStocks();
-			Set<Integer> buyingStocks = ftpOp.getBuyingStocks();
-			Set<Integer> tradingStocks = findTradingStocks(sellingStocks, buyingStocks);
-
-			/* get trading stocks' last day close price */
-
-			/* do the aggregation auction if tradingStocks has content */
-
-			/* update the matrix screen */
 		}
 	}
 
